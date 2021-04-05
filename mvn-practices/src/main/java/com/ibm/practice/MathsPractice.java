10fd39e1a6114b7cf2d017fb874821cead6e63f6
Tarishi Geetey <tgeeteyt@gmail.com>
2021-04-05 22:38:33 +0530

    My practice git

---
+++ mvn-practices/src/main/java/com/ibm/practice/MathsPractice.java
@@ -0,0 +1,23 @@
+package com.ibm.practice;
+
+public class MathsPractice {
+	
+	public MathsPractice() {
+		// TODO Auto-generated constructor stub
+	}
+	public int sum(int a, int b) {
+		return a+b;
+	}
+	
+	public int subs(int a, int b) {
+		return a-b;
+	}
+	
+	public int div(int a, int b) {
+		return a/b;
+	}
+	public int square(int a ) {
+		return a*a;
+	}
+
+}
---
+++ mvn-practices/src/test/java/com/ibm/practice/TestMathsPractice.java
@@ -0,0 +1,49 @@
+
+package com.ibm.practice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TestMathsPractice {
+	
+	private static MathsPractice maths;
+	@BeforeAll
+	public static void setup() {
+		maths = new MathsPractice();
+	}
+	
+	@Test
+	public void testAdd() {
+		assertEquals(10, maths.sum(5,5));
+	}
+	
+	@Test
+	public void testSub() {
+		assertEquals(10, maths.subs(15,5));
+	}
+	
+	@Test
+	public void testDiv() {
+		assertEquals(10, maths.div(10,1));
+	}
+	
+	@Test
+	public void testZeroDiv() {
+		assertThrows(ArithmeticException.class, () -> maths.div(2, 0));
+	}
+	
+	@Test
+	public void testSquare() {
+		assertEquals(25, maths.square(5));
+	}
+	
+	@Test
+	public void testIncorrectSquare() {
+		assertNotEquals(10, maths.square(3));
+	}
+
+}
